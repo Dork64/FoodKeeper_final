ddd5465fe87075f0c0477e0f515f65c4216c580d
elnararavilova <elnararavilova51@gmail.com>
2024-11-23 17:55:24 +0300

    разметка элементов списка

---
+++ app/src/main/java/com/example/foodkeeper_final/models/ShoppingItem.kt
@@ -0,0 +1,9 @@
+package com.example.foodkeeper_final.models
+
+
+data class ShoppingItem(
+    var id: String = "",
+    var name: String = "",
+    val category: String = "",
+    val imageUrl: String = ""
+)
