86bc0746ca56d8cf1ea9ece27b356b0cdaaf535a
Иван <dork641@yandex.ru>
2024-11-23 17:48:46 +0300

    MainActivity

--- app/src/main/java/com/example/foodkeeper_final/MainActivity.kt
+++ app/src/main/java/com/example/foodkeeper_final/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.foodkeeper_final
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.foodkeeper_final.fragments.ShoppingListFragment
+import androidx.fragment.app.Fragment
+import com.example.foodkeeper_final.fragments.FridgeFragment
+import com.example.foodkeeper_final.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        FirebaseApp.initializeApp(this)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        loadFragment(ShoppingListFragment<Any>())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.shoppingList -> {
+                    loadFragment(ShoppingListFragment<Any>())
+                    true
+                }
+                R.id.fridge -> {
+                    loadFragment(FridgeFragment())
+                    true
+                }
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+
+                else -> {
+                    loadFragment(ShoppingListFragment<Any>())
+                    true
+                }
+            }
         }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
